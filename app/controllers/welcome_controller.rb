daad2da1aa9bed15faea9e0171a45d211d73382d
Amanda Mendez <amandaag39@gmail.com>
2024-03-25 16:20:42 -0500

    added logged in landing page and some links to hamburger drawer for better navigation and ux

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to readings_path
+      render 'welcome/logged_in' # Renders the logged_in.html.erb view for authenticated users
     end
   end
 end
