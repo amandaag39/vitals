e08e135c15d0a193479c063e7913d35ce6c1f28e
Amanda Mendez <amandaag39@gmail.com>
2024-04-06 13:07:35 -0500

    added markdown processing with redcarpet gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
     include Pagy::Frontend
+
+    def markdown_to_html(markdown_content)
+        renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+        markdown = Redcarpet::Markdown.new(renderer, extensions = {autolink: true, no_intra_emphasis: true})
+        markdown.render(markdown_content).html_safe
+    end
 end
